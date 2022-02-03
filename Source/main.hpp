d9d1dc13a693a8ace154d402ebd8fef37b18bec4
mdarius13 <mdarius13@icloud.com>
2022-02-03 18:48:58 +0100

    Fix Linking error

--- Source/main.hpp
+++ Source/main.hpp
@@ -30,11 +30,6 @@
 #ifndef PAUSE_MAIN_HPP
 #define PAUSE_MAIN_HPP
 
-/* Name Mangling */
-#ifdef __cplusplus
-    extern "C" {
-#endif
-
 /* Include Directive */
 #pragma region "Include Directive"
 #include <stdio.h>
@@ -52,12 +47,10 @@
 
 /* Methods */
 #pragma region "Methods"
-int main(void);
+extern "C"
+{
+    int main(void);
+}
 #pragma endregion
-
-/* Name Mangling */
-#ifdef __cplusplus
-    }
-#endif
 
 #endif /* PAUSE_MAIN_HPP */
